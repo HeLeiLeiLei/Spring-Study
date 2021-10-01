28706209f8e860c95d4e801accb7468e40ec7108
HeLeiLeiLei <3233583450@qq.com>
2021-10-01 23:54:33 +0800

    transaction 以aop的思想实现

--- spring-11-transaction/src/main/java/com/hl/mapper/UserMapperImpl.java
+++ spring-11-transaction/src/main/java/com/hl/mapper/UserMapperImpl.java
@@ -7,20 +7,18 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-
+//@Component
 public class UserMapperImpl implements UserMapper {
 
-
+    //@Autowired
     private SqlSessionTemplate sqlSession;
-
     public void setSqlSession(SqlSessionTemplate sqlSession) {
         this.sqlSession = sqlSession;
     }
 
     public List<User> queryUsers() {
-        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
-        List<User> userList = mapper.queryUsers();
-        User user = new User(6, "小⑤", "888888");
+        List<User> userList = sqlSession.getMapper(UserMapper.class).queryUsers();
+        User user = new User(5, "小⑤", "888888");
         addUser(user);
         deleteUser(5);
         return userList;
--- spring-11-transaction/src/test/java/MyTest.java
+++ spring-11-transaction/src/test/java/MyTest.java
@@ -1,25 +1,29 @@
+
 import com.hl.mapper.UserMapper;
 import com.hl.pojo.User;
-
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-
-import java.io.IOException;
 import java.util.List;
 
 public class MyTest {
 
-    @Test
-    public void Test() throws IOException {
+//    @Test
+//    public void Test(){
+//       ApplicationContext context =
+//               new ClassPathXmlApplicationContext("applicationContext.xml");
+//
+//       UserMapper userMapper = (UserMapper) context.getBean("userMapper");
+//       System.out.println(userMapper.queryUsers());
+//   }
+
+    public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext.xml");
+
         UserMapper userMapper = (UserMapper) context.getBean("userMapper");
-        List<User> userList = userMapper.queryUsers();
-        for (User user : userList) {
-            System.out.println(user);
-        }
+        System.out.println(userMapper.queryUsers());
     }
 
 }
